79ff1b13d42c81febeb3353814bd6920761e7bf0
Marius Karnauskas <marius.karnauskas@gmail.com>
2016-03-29 07:11:42 +0100

    config load fix

--- src/main/java/lt/nkts/cassandra/ConsulSeedProvider.java
+++ src/main/java/lt/nkts/cassandra/ConsulSeedProvider.java
@@ -4,6 +4,8 @@ import com.ecwid.consul.v1.ConsulClient;
 import com.ecwid.consul.v1.Response;
 import com.ecwid.consul.v1.kv.model.GetValue;
 import org.apache.cassandra.locator.SeedProvider;
+import org.apache.cassandra.config.Config;
+import org.apache.cassandra.config.DatabaseDescriptor;
 import org.json.simple.parser.JSONParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,35 +22,44 @@ import java.util.Map;
 public class ConsulSeedProvider implements SeedProvider {
 
     private static final Logger logger = LoggerFactory.getLogger(ConsulSeedProvider.class);
-    private String prefix = "cassandra/seeds";
-    private URL consulUrl;
     private ConsulClient client;
     private JSONParser jsonParser = new JSONParser();
 
-    public ConsulSeedProvider(final Map<String, String> args) {
+    public ConsulSeedProvider(final Map<String, String> args) {}
+
+    public List<InetAddress> getSeeds() {
+        Config config = null;
+        URL consulUrl = null;
+        String prefix = "";
 
         try {
-            consulUrl = new URL(args.get("consul_url"));
-            logger.info("Consul url: {}", consulUrl);
+            config = DatabaseDescriptor.loadConfig();
+            logger.debug(config.seed_provider.toString());
         } catch (Exception e) {
-            logger.warn("Error getting consul_url: {}", e.getMessage());
+            logger.error(e.getMessage());
         }
 
         try {
-            prefix = args.get("consul_prefix");
+            consulUrl = new URL(config.seed_provider.parameters.getOrDefault("consul_url", "http://localhost:8500/"));
+            logger.debug("Consul url: {}", consulUrl);
+        } catch (Exception e) {
+            logger.error("Error getting consul_url: {}", e.getMessage());
+        }
+
+        try {
+            prefix = config.seed_provider.parameters.getOrDefault("consul_prefix", "cassandra/seeds");
             logger.info("Consul prefix: {}", prefix);
         } catch (Exception e) {
             logger.warn("Error getting consul_prefix: {}", e.getMessage());
         }
 
-    }
-
-    public List<InetAddress> getSeeds() {
-
         client = new ConsulClient(String.format("%s:%s", consulUrl.getHost(), consulUrl.getPort()));
 
         Response response = client.getKVValues(prefix);
         List all = (ArrayList<GetValue>) response.getValue();
+        if (all == null) {
+            return new ArrayList();
+        }
         List<InetAddress> seeds = new ArrayList<InetAddress>(all.size());
 
         for (Object gv : all) {
